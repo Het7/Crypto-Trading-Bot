0e7d8b6bb8bcb62fc773c6163f1ee07b0fa4006e
Het7 <hetparikh7@gmail.com>
2021-10-20 21:15:03 -0400

    Finished websocket configuration

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import websocket, json, pprint
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
+closes = []
+
 def on_open(ws):
     print('opened connection')
 
@@ -9,6 +11,7 @@ def on_close(ws):
     print('closed connection')
 
 def on_message(ws, message):
+    global closes
     print('received message')
     json_message = json.loads(message)
     pprint.pprint(json_message)
@@ -20,6 +23,9 @@ def on_message(ws, message):
 
     if is_candle_closed:
         print("candle closed at {}".format(close))
+        closes.append(float(close))
+        print("closes")
+        print(closes)
 
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
